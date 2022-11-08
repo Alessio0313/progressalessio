4c0c0d326250deae790b39dac47bb03f3867713e
Alessio0313 <113989745+Alessio0313@users.noreply.github.com>
2022-11-08 12:07:31 +0100

    Create progress.js

---
+++ progress.js
@@ -0,0 +1,39 @@
+console.log(`Szeretnék megtanulni programozni.`)
+
+let a = 'javascript'
+console.log(a)
+
+let b = 10*12
+console.log(b)
+
+let c = 12
+if (c % 2 == 0) {
+    console.log('páros')
+} else {
+    console.log('páratlan')
+}
+
+  let d = 1
+ while (d <= 10) {
+ console.log('hazudni bűn')
+ d++
+ }
+
+ let e = 10
+ while (e <= 500) {
+     console.log(e)
+    e += 2
+ }
+
+
+  function sayhello(a) {
+   return 'hello' + a
+ }
+
+ console.log(sayhello('alessio'))
+
+ const osszeadas = (num1, num2) => num1 + num2
+
+ console.log(osszeadas(1, 2))
+
+ 
